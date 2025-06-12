1adacfb3bd8afb7aedb2984e4c3ca32947c7d3df
kawilliams <kawilliams@davidson.edu>
2025-06-12 09:35:22 -0400

    Adding skeleton

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Global objects go here (outside of any functions)
+
+
+/**
+ * Load data from CSV file asynchronously and render charts
+ */
+
+
+
+/**
+ * Use bar chart as filter and update scatter plot accordingly
+ */
